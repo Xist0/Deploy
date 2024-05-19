7ec6b6a0cf1f9660da53007f7fe638efe7d43736
Xist0 <x1spridcherd@gmail.com>
2024-05-19 10:56:49 +0500

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://http://192.168.1.157:5000',
+        changeOrigin: true,
+        secure: false, 
+      },
+    },
+    https: {
+      key: './CRMServe-private.key',
+      cert: './CRMServe.crt',
+    }
+  },
   plugins: [react()],
 })
