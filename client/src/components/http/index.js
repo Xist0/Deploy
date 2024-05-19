697d4cdfbb9a3b4d48343f09324ee454a7004ae5
Xist0 <x1spridcherd@gmail.com>
2024-05-19 12:19:30 +0500

    1

--- client/src/components/http/index.js
+++ client/src/components/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://192.168.1.157:5000/api/login'
+export const API_URL = 'http://192.168.1.157:5000/api'
 
 const $api = axios.create({
     withCredentials: true,
