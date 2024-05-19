4ca7e057c322d3343627643ac057e753041c7cf2
Xist0 <x1spridcherd@gmail.com>
2024-05-19 12:14:15 +0500

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.1.157:50000',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+
+  },
   plugins: [react()],
 })
