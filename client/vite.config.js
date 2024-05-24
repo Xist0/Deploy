014171eaad0c661fdf71f641d971e31af10a78a5
xisto <x1spridcherd@gmail.com>
2024-05-24 16:50:45 +0500

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', 
+        target: 'http://192.168.1.157:5000', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
