90471a1ee8f4e56eaf76dbfb3d84b44a666c7fdc
xisto <x1spridcherd@gmail.com>
2024-05-24 11:52:56 +0500

    1

--- server/server.js
+++ server/server.js
@@ -41,9 +41,9 @@ app.use('/api', router);
 app.use(errorMiddleware);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
