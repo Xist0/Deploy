8ba238b96fbe23dbc8924e1db093cfc3f13784dd
xisto <x1spridcherd@gmail.com>
2024-05-24 16:00:56 +0500

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,15 +16,11 @@ export default defineConfig({
     port: 24,
     proxy: {
       "/api": {
-        target: "https://192.168.1.211:4000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
         ws: true,
       },
-    },
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './CRMServe-private.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, './CRMServe.crt')),
-    },
-  },
+    }
+  }
 });
