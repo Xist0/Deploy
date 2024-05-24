66b23e7c61e3a6ab21ee6a1e53eef22daca138ee
xisto <x1spridcherd@gmail.com>
2024-05-24 10:41:28 +0500

    1

--- client/src/components/http/index.js
+++ client/src/components/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = process.env.API_URL || 'https://order.service-centr.com:5000/api';
+export const API_URL = process.env.API_URL || 'http://localhost:5000/api';
 
 const $api = axios.create({
     withCredentials: true,
