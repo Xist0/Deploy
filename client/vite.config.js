396b6b90e718bbe40ae466b4759f70cfaba4704a
xisto <x1spridcherd@gmail.com>
2024-05-24 16:31:56 +0500

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import macrosPlugin from 'vite-plugin-babel-macros';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [macrosPlugin(), react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
     },
   },
-  server: {
-    port: 24,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    }
-  }
-});
+})
