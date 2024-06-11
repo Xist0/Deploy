f1ea5be1613705bc9a573f5bed9de9d12d209f32
xisto <x1spridcherd@gmail.com>
2024-06-11 12:50:09 +0500

    1

--- client/src/components/component/LoginForm.jsx
+++ client/src/components/component/LoginForm.jsx
@@ -1,67 +1,80 @@
 import React, { useContext, useState } from 'react';
 import { Context } from '../../main';
 import { observer } from 'mobx-react-lite';
-import './login.css';
+import './login.css'
+import { IoEyeSharp } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa6";
+
 
 function LoginForm() {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [role, setRole] = useState('');
-    const { store } = useContext(Context);
-    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    const [isFieldsValid, setIsFieldsValid] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // Новое состояние
 
-    const handleRegistration = async () => {
-        try {
-            await store.registration(login, password, role);
-            handleMessage('Пользователь успешно создан');
-            clearForm();
-        } catch (error) {
-            handleMessage(`Ошибка создания пользователя: ${error.message}`);
+    const { store } = useContext(Context);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        if (name === 'login') setLogin(value);
+        if (name === 'password') setPassword(value);
+        if (login.trim() !== '' && password.trim() !== '') {
+            setIsFieldsValid(true);
+        } else {
+            setIsFieldsValid(false);
         }
     };
 
-    const handleMessage = (msg) => {
-        setMessage(msg);
-        setTimeout(() => {
-            setMessage('');
-        }, 3000); 
+    const handleLogin = async () => {
+        try {
+            const response = await store.login(login, password, role);
+            if (response.error) {
+                setError(response.error);
+            } else {
+                setError('');
+            }
+        } catch (error) {
+            setLogin('');
+            setPassword('');
+            setError(error.message);
+        }
     };
 
-    const clearForm = () => {
-        setLogin('');
-        setPassword('');
-        setRole('');
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(prevState => !prevState); // Функция для переключения видимости пароля
     };
 
     return (
         <div className='container-login'>
-            <h1>Создание новой учётной записи</h1>
-
+            <h1>Авторизация</h1>
             <input
-                onChange={e => setLogin(e.target.value)}
+                onChange={handleInputChange}
                 value={login}
+                name="login"
                 type="text"
                 placeholder='Логин'
+                className={!isFieldsValid && login.trim() === '' ? 'invalid' : ''}
             />
-            <input
-                onChange={e => setPassword(e.target.value)}
-                value={password}
-                type="password"
-                placeholder='Пароль'
-            />
-            <select onChange={(e) => setRole(e.target.value)} value={role}>
-                <option value="">Выберите роль</option>
-                <option value="Администратор">Администратор</option>
-                <option value="Приём">Приём</option>
-                <option value="Отправка">Отправка</option>
-                <option value="Стажёр">Стажёр</option>
-                <option value="Мастер">Мастер</option>
-                <option value="Выдача">Выдача</option>
-                <option value="Курьер">Курьер</option>
-                <option value="ТехАдмин">ТехАдмин</option>
-            </select>
-            <h1>{message}</h1>
-            <button onClick={handleRegistration}>Создать</button>
+            <div className="password-container">
+                <input
+                    onChange={handleInputChange}
+                    value={password}
+                    name="password"
+                    type={isPasswordVisible ? 'text' : 'password'} 
+                    placeholder='Пароль'
+                    className={!isFieldsValid && password.trim() === '' ? 'invalid' : ''}
+                />
+                {isPasswordVisible ? (
+                    <FaEyeSlash onClick={togglePasswordVisibility} />
+                ) : (
+                    <IoEyeSharp onClick={togglePasswordVisibility} />
+                )}
+
+            </div>
+            <button onClick={handleLogin} disabled={!isFieldsValid} className={!isFieldsValid ? 'button-error' : ''}>Войти</button>
+            {error && <div className="error">{error}</div>}
         </div>
     );
 }
