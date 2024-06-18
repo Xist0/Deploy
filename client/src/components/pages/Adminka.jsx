141f5498a529b776869894068e769e17cba7397b
xisto <x1spridcherd@gmail.com>
2024-06-18 13:27:16 +0500

    1

--- client/src/components/pages/Adminka.jsx
+++ client/src/components/pages/Adminka.jsx
@@ -10,7 +10,7 @@ function Adminka() {
         <div className="container-box">
             <RegisterForm  />
             <UserList />
-            <PrintComponent/>
+            {/* <PrintComponent/> */}
         </div>
     );
 }
